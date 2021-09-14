268cfeca169a274b02c30c1d881c116d1fff5c5a
Facundo Arancet Iza <facuarancet97@gmail.com>
2021-09-13 23:44:45 -0300

    [Refactor] new profile test constants extraction

--- Contacts/BusinessLogic.Test/ProfileTests.cs
+++ Contacts/BusinessLogic.Test/ProfileTests.cs
@@ -6,16 +6,22 @@ namespace BusinessLogic.Test
     [TestClass]
     public class ProfileTests
     {
+        private const string John = "John";
+        private const string Doe = "Doe";
+        private const int Age23 = 23;
+        private const string PhoneNumberSample = "+59899999999";
+        private const string SomeStreetAddress = "SomeStreet 1111";
+
         [TestMethod]
         public void NewProfile()
         {
             var aProfile = new Profile()
             {
-                FirstName = "John",
-                LastName = "Doe",
-                Age = 23,
-                PhoneNumber = "+59899999999",
-                Address = "Somestreet 1111",
+                FirstName = John,
+                LastName = Doe,
+                Age = Age23,
+                PhoneNumber = PhoneNumberSample,
+                Address = SomeStreetAddress,
             };
             Assert.IsNotNull(aProfile);
         }
