4db93dc28cf02904f7d98720cbf0b54ff987a153
Facundo Arancet Iza <facuarancet97@gmail.com>
2021-09-13 23:29:12 -0300

    [Green] new profile implementation

---
+++ Contacts/BusinessLogic/Profile.cs
@@ -0,0 +1,15 @@
+﻿namespace BusinessLogic
+{
+    public class Profile
+    {
+        public Profile()
+        {
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+    }
+}
