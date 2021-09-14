cf2b76984942e8cf781663fdf698d05e741c99d6
Facundo Arancet Iza <facuarancet97@gmail.com>
2021-09-14 00:22:21 -0300

    [Green] first name max length, min length and not null tests implementation

--- Contacts/BusinessLogic/Profile.cs
+++ Contacts/BusinessLogic/Profile.cs
@@ -1,12 +1,27 @@
-﻿namespace BusinessLogic
+﻿using System;
+
+namespace BusinessLogic
 {
     public class Profile
     {
+        public const int FirstNameMinLength = 2;
+        public const int FirstNameMaxLength = 30;
+        private string firstName;
+
         public Profile()
         {
         }
 
-        public string FirstName { get; set; }
+        public string FirstName { 
+            get => firstName; 
+            set { 
+                if(value?.Length < FirstNameMinLength || value?.Length > FirstNameMaxLength)
+                {
+                    throw new BusinessLogicException("First name length must be between 2 and 30");
+                }
+                firstName = value ?? throw new ArgumentNullException("First name cannot be null");
+            } 
+        }
         public string LastName { get; set; }
         public int Age { get; set; }
         public string PhoneNumber { get; set; }
