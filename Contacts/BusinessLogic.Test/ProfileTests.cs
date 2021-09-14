6309c159347de6c5a0b0bedfe80c0b8c4d826b21
Facundo Arancet Iza <facuarancet97@gmail.com>
2021-09-13 23:27:09 -0300

    [Red] new profile test

--- Contacts/BusinessLogic.Test/ProfileTests.cs
+++ Contacts/BusinessLogic.Test/ProfileTests.cs
@@ -4,11 +4,20 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace BusinessLogic.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class ProfileTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void NewProfile()
         {
+            var aProfile = new Profile()
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Age = 23,
+                PhoneNumber = "+59899999999",
+                Address = "Somestreet 1111",
+            };
+            Assert.IsNotNull(aProfile);
         }
     }
 }
