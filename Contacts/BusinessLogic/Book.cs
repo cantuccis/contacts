eae6d1746e3a0fa4c1b8d771d508e97421cda7a0
Facundo Arancet Iza <facuarancet97@gmail.com>
2021-09-21 20:38:39 -0300

    [Green] Book implementation

---
+++ Contacts/BusinessLogic/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessLogic
+{
+    public class Book
+    {
+        private IList<Profile> profiles;
+        public Book()
+        {
+            profiles = new List<Profile>();
+        }
+
+        public string Name { get; set; }
+
+        public bool Contains(Profile someProfile)
+        {
+            return profiles.Contains(someProfile);
+        }
+
+        public void Add(Profile someProfile)
+        {
+            profiles.Add(someProfile);
+        }
+
+        public void Remove(Profile someProfile)
+        {
+            profiles.Remove(someProfile);
+        }
+
+        public IList<Profile> GetAllProfiles()
+        {
+            return new List<Profile>(profiles);
+        }
+    }
+}
