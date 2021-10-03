3b3c09fbb91a25d0e8d7178153a53ccb0fcd15fb
Facundo Arancet Iza <facuarancet97@gmail.com>
2021-10-03 08:37:38 -0300

    [Red] User tests

---
+++ Contacts/BusinessLogic.Test/UserTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusinessLogic.Test
+{
+    [TestClass]
+    public class UserTests
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            User user = new User();
+
+            Assert.IsNotNull(user);
+        }
+
+        [TestMethod]
+        public void UsernameTest()
+        {
+            User user = new User()
+            {
+                Username = "username"
+            };
+
+            var username = user.Username;
+
+            Assert.AreEqual("username", username);
+        }
+
+        [TestMethod]
+        public void PasswordTest()
+        {
+            User user = new User()
+            {
+                Password = "abc"
+            };
+
+            var password = user.Password;
+
+            Assert.AreEqual("abc", user.Password);
+        }
+    }
+}
