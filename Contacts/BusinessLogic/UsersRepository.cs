43e1209de40d4f9d6c6ba04c6e67964718f1e5a6
Facundo Arancet Iza <facuarancet97@gmail.com>
2021-10-03 09:39:03 -0300

    [Red] UsersRepository skelleton created with visual studio code auto-generation

---
+++ Contacts/BusinessLogic/UsersRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessLogic
+{
+    public class UsersRepository
+    {
+        public UsersRepository()
+        {
+        }
+
+        public int Count { get; }
+
+        public void Add(User adminUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Get(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(User newPasswordAdmin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
