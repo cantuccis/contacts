0b380e0f65ee081085965fc1f6b371c2209af49b
Facundo Arancet Iza <facuarancet97@gmail.com>
2021-10-02 22:14:23 -0300

    Added application settings to provide testing data on app init

---
+++ Contacts/ContactsUI/ApplicationSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsUI
+{
+    class ApplicationSettings
+    {
+        public const string TestingDataEnabled = "TestingDataEnabled";
+        public const string TestingBooksCount = "TestingBooksCount";
+        public const string TestingProfilesCount = "TestingProfilesCount";
+
+    }
+}
