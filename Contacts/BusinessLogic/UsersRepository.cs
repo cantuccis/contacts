49654d914087cac497f4b9a074fd28bc44180a51
Facundo Arancet Iza <facuarancet97@gmail.com>
2021-10-03 09:53:44 -0300

    [Green] users repo impl

--- Contacts/BusinessLogic/UsersRepository.cs
+++ Contacts/BusinessLogic/UsersRepository.cs
@@ -1,49 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using BusinessLogic.Exceptions;
 
 namespace BusinessLogic
 {
     public class UsersRepository
     {
+
+        IList<User> collection;
+
         public UsersRepository()
         {
+            collection = new List<User>();
         }
 
-        public int Count { get; }
+        public int Count { get => collection.Count; }
 
-        public void Add(User adminUser)
+        public void Add(User user)
         {
-            throw new NotImplementedException();
+            if (user is null)
+                throw new ArgumentNullException(nameof(user));
+            if (Exists(user.Username))
+                throw new BusinessLogicException("User already exsists");
+            
+            collection.Add(user);
         }
 
         public bool Exists(string username)
         {
-            throw new NotImplementedException();
+            return collection.Any(user => user.Username == username);
         }
 
         public User Get(string username)
         {
-            throw new NotImplementedException();
+            if (!Exists(username))
+                throw new BusinessLogicException("User does not exsist");
+            return collection.First(user => user.Username == username);
         }
 
-        public void Update(User newPasswordAdmin)
+        public void Update(User user)
         {
-            throw new NotImplementedException();
+            Remove(user.Username);
+            Add(user);
         }
 
         public void Remove(string username)
         {
-            throw new NotImplementedException();
+            collection = collection.Where(user => user.Username != username).ToList();
         }
 
         public IList<User> GetAll()
         {
-            throw new NotImplementedException();
+            return new List<User>(collection); //return a copy, not the actual list!
         }
 
         public void Clear()
         {
-            throw new NotImplementedException();
+            collection.Clear();
         }
     }
 }
