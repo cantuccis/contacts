c390b107d3fd0aa08c148375606fb507294fe62e
Facundo Arancet Iza <facuarancet97@gmail.com>
2021-10-03 11:15:58 -0300

    [Green] User deep clone implementation

--- Contacts/BusinessLogic/User.cs
+++ Contacts/BusinessLogic/User.cs
@@ -1,4 +1,6 @@
-﻿namespace BusinessLogic
+﻿using System;
+
+namespace BusinessLogic
 {
     public class User
     {
@@ -9,5 +11,15 @@
         public string Username { get; set; }
         public string Password { get; set; }
         public bool IsAdmin { get; set; }
+
+        public User DeepClone()
+        {
+            return new User()
+            {
+                Username = this.Username,
+                Password = this.Password,
+                IsAdmin = this.IsAdmin
+            };
+        }
     }
 }
