eed3fea8e07d23adfd5d1fa654ae9f0ed5ff7b15
Facundo Arancet Iza <facuarancet97@gmail.com>
2021-10-03 08:39:28 -0300

    [Green] User tests passing

---
+++ Contacts/BusinessLogic/User.cs
@@ -0,0 +1,12 @@
+﻿namespace BusinessLogic
+{
+    public class User
+    {
+        public User()
+        {
+        }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
