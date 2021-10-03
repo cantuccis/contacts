f54c6fc0dd6df42864f30c7ad3896802e5dc8a58
Facundo Arancet Iza <facuarancet97@gmail.com>
2021-10-03 10:22:15 -0300

    [Red] Credentials struct and credentials manager skeleton created

---
+++ Contacts/BusinessLogic/CredentialsManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BusinessLogic
+{
+    public class CredentialsManager
+    {
+        public CredentialsManager(UsersRepository usersRepo, User admin)
+        {
+        }
+
+        public bool IsLogged { get; set; }
+
+        public void Login(User admin)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
