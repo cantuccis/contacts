aa7a2ff51ed380bde8015d24b6fa6d56ffbadd2b
Facundo Arancet Iza <facuarancet97@gmail.com>
2021-10-12 02:04:55 -0300

    Added mini profile user control

---
+++ Contacts/ContactsUI/Controls/ProfileControls/MiniProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContactsUI.Controls.ProfileControls
+{
+    public partial class MiniProfile : UserControl
+    {
+        public MiniProfile(string imagePath, string text)
+        {
+            InitializeComponent();
+            this.picture.Image = new Bitmap(imagePath);
+            this.label.Text = text;
+            Refresh();
+        }
+    }
+}
